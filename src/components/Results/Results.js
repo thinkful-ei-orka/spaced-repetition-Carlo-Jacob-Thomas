b24cfeae60fbeac85964cb8b2d975b742dc9e616
Thomas Davis <ThomasDavisA@gmail.com>
2020-07-31 11:56:37 -0700

    more css styling

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -19,7 +19,7 @@ class Results extends React.Component {
         return (
             <div className="DisplayFeedback">
                 <p className="results-p">The correct translation for <span className='word-strong'>{this.props.original}</span> was <span className='word-strong'>{this.props.answer}</span> and you chose <span className='word-strong'>{this.props.guess}</span>!</p>
-                <Button onClick={this.props.onNextWordClick}>
+                <Button className='nextword-button' onClick={this.props.onNextWordClick}>
                 Try another word!
                 </Button>
             </div>
